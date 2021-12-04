a0e946306c53e888cf22a549863abacc25f399dc
zenlex <erichrkeil@gmail.com>
2021-12-03 19:05:49 -0600

    day 3 complete

--- 2021/03/bd.js
+++ 2021/03/bd.js
@@ -1,11 +1,17 @@
 const fs = require('fs');
 
-let ship = { powcons: 0, epsilon: 0, gamma: 0, };
+let ship = { powcons: 0, epsilon: 0, gamma: 0, life: 0, oxygen: 0, coscrub: 0 };
 
 function calcPower() {
   ship.powcons = ship.epsilon * ship.gamma;
   console.log(`Current Power Consumption: ${ship.powcons}`)
   return ship.powcons;
+}
+
+function calcLife() {
+  ship.life = ship.oxygen * ship.coscrub;
+  console.log(`Current Life Support: ${ship.life}`)
+  return ship.life;
 }
 
 fs.readFile('input.txt', 'utf-8', (err, data) => {
@@ -15,26 +21,76 @@ fs.readFile('input.txt', 'utf-8', (err, data) => {
 
   // transform input data into array of lines - each line 5 bits
   const lines = data.split('\n').filter(item => item !== '');
-  const rows = lines.length;
+  
   const bits = lines[0].length;
-  console.log(`${rows} rows of codes found`);
   // transform lines into columns
-  const columns = [];
-  for (let i = 0; i < bits; i++) {
-    columns[i] = [];
-    lines.forEach(code => columns[i].push(parseInt(code[i])));
+  function getColumns(rows){
+    const cols = [];
+    for (let i = 0; i < bits; i++) {
+      cols[i] = [];
+      rows.forEach(code => cols[i].push(parseInt(code[i])));
+    }
+    return cols;
   }
-  // calculate most common bit in column array (gama rate)
-  const gammaBits = columns.map(col => {
-    return col.filter(bit => bit).length > (rows / 2)
-      ? 1 : 0
-  }).join('');
+
+  const columns = getColumns(lines);
+
+  function calcBitMask(cols, lcb) {
+    const rows = cols[0].length;
+    let bitMask = cols.map(col => {
+      const ones = col.filter(bit => bit);
+      if (lcb) {
+        return (ones.length >= (rows / 2) ? 0 : 1)
+      }
+      return (ones.length >= (rows / 2) ? 1 : 0)
+    })
+    return bitMask;
+  }
+  // calculate most common bit in column array (gamma rate)
+  const gammaBits = calcBitMask(columns).join('');
   ship.gamma = parseInt(gammaBits, 2);
 
-  //calculate epsilon rate as opposite of gamma rate
-  const epsilonBits = gammaBits.split('').map(bit => parseInt(bit) ? 0 : 1).join('')
+  // calculate least common bit in column array (epsilon rate)
+  const epsilonBits = calcBitMask(columns, true).join('');
   ship.epsilon = parseInt(epsilonBits, 2)
-  //convert epsilon and gamma rates to decimal, multiply, and return answer
+
+  // calculate o2 gen
+  function calcOx() {
+    let oxBits = lines.slice();
+
+    for (let i = 0; i < columns.length; i++) {
+      let bitMask = calcBitMask(getColumns(oxBits)).join('');
+
+      oxBits = oxBits.filter(code => code[i] === bitMask[i]);
+
+      if (oxBits.length === 1) {
+        console.log(`Ship oxygen calculated at: ${parseInt(oxBits[0], 2)}`)
+        return parseInt(oxBits[0], 2);
+      }
+    }
+  };
+
+  // calculate co2 scrub rate
+  function calcCoScrub() {
+    let coBits = lines.slice()
+    
+    for (let i = 0; i < columns.length; i++) {
+      let bitMask = calcBitMask(getColumns(coBits), true).join('');
+
+      coBits = coBits.filter(code => code[i] === bitMask[i]);
+
+      if (coBits.length === 1) {
+        console.log(`Co2 Scrubber calculated at: ${parseInt(coBits[0], 2)}`)
+        return parseInt(coBits[0], 2);
+      }
+    }
+  }
+
+
+  // ship status update
+  ship.oxygen = calcOx();
+  ship.coscrub = calcCoScrub();
   calcPower();
+  calcLife();
 
 })
