6563d132bd05a9d0e9adf199d249d59b827e1918
zenlex <erichrkeil@gmail.com>
2021-12-05 01:29:50 -0600

    WIP pt2 example passes final doesn't

--- 2021/05/vents.js
+++ 2021/05/vents.js
@@ -1,5 +1,4 @@
 const fs = require('fs');
-const { isRegExp } = require('util/types');
 
 fs.readFile('input.txt', 'utf-8', (err, data) => {
   if (err) console.error(err);
@@ -10,7 +9,6 @@ fs.readFile('input.txt', 'utf-8', (err, data) => {
     .map(val => parseInt(val))
     )
     );
-  console.log(lines);
   // create array from 0,0 to max x and max y to track interactions
   const xs = [];
   const ys = [];
@@ -22,10 +20,11 @@ fs.readFile('input.txt', 'utf-8', (err, data) => {
   }
   const cols = Math.max(...xs) + 1;
   const rows = Math.max(...ys) + 1;
+  console.log(`cols: ${cols}, rows: ${rows}`)
   for (let i = 0; i < cols * rows; i++){
     vents.push(0);
   }
-
+  console.log(vents.length);
   //for each line in lines traverse the state array increasing value at each point
   for (let line of lines) {
     let x0 = line[0][0];
@@ -36,24 +35,36 @@ fs.readFile('input.txt', 'utf-8', (err, data) => {
     if(x0 === x1){ // vertical line
       const min = Math.min(y0, y1) * cols + x0;
       const max = Math.max(y0, y1) * cols + x0;
+      // console.log(`vertical line from ${min} to ${max}`)
       for(let i = min; i <= max; i += cols){
-        console.log(`drawing a vertical line from ${min} to ${max}`)
         vents[i] += 1;
       }     
+      continue;
     } 
     if(y0 === y1){ // horizontal line
       const offset = y0 * cols;
       const min = Math.min(x0, x1) + offset;
       const max = Math.max(x0, x1) + offset;
+      // console.log(`horiz line from (${min} to ${max})`)
       for(let i = min; i <= max; i++){
-        console.log(`drawing horizontal line from ${min} to ${max}`)
         vents[i] += 1;
       }
+      continue;
     }
-    //ignore diagonal line
     
+    // diagonal line - always draw left to right (xmin -> xmax)
+    const xmin = Math.min(x0, x1);
+    const ymin = xmin === x0 ? y0 : y1;
+    const xmax = Math.max(x0, x1);
+    const ymax = xmax === x0 ? y0 : y1;
+    const start = ymin * cols + xmin;
+    const end = ymax * cols + xmax;
+    const offset = start < end ? cols + 1 : -cols + 1;
+    console.log(`start: ${start}, end: ${end}, offset: ${offset}`)
+    for (let i = start; i != end; i += offset){
+      vents[i] += 1;
+    }
   }
-  console.log(vents);
   
   //count number of values >= 2
   console.log('points >= 2: ', vents.filter(val => val >= 2).length);
