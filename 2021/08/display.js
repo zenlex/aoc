a4b7ab9de3497fb470bd6c5cf80f6c51e8cd01a3
zenlex <erichrkeil@gmail.com>
2021-12-11 22:38:20 -0600

    08 complete

--- 2021/08/display.js
+++ 2021/08/display.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const { resourceLimits } = require('worker_threads');
 const segsByLen = new Map([[2, 1], [3, 7], [4, 4], [5, [2, 3, 5]], [6, [0, 6, 9]], [7, 8]])
 
 function part1(data) {
@@ -11,27 +12,31 @@ function part1(data) {
     }
     return count;
   }, 0)
-  console.log('RESULT: ', result);
+  console.log('Part 1 RESULT: ', result);
   return result;
 }
 
 function part2(data) {
   console.log('running part2');
   const displays = parse(data);
+  let sum = 0;
   const outputs = [];
   for (const display of displays.entries()) {
     const input = display[0];
     const output = display[1];
     const legend = getLegend(input);
-    const result = getOutput(output, legend); 
+    const result = getOutput(output, legend);
+    sum += result;
   }
-  return 5353; //stub
+  console.log('PART 2 Result:', sum);
+  return sum; //stub
 }
 
 (async () => {
   if (require.main === module) {
     const data1 = await getData('input1.txt');
     part1(data1);
+    part2(data1);
   }
 })();
 
@@ -64,28 +69,107 @@ function isAnagram(str1, str2) {
 }
 
 function getLegend(strarr) {
-  result = new Map();
+  const result = new Map();
+
   if (!strarr) {
     for (let i = 0; i < 10; i++) {
       result.set(i, null);
     }
+    return result;
   }
 
-  // TODO: Logic here should take the ioPair and sort which set of letters = which digit and set the values in keyvals to [digit, letter] pairs. 
-  return result;
+  for (let i = 0; i < strarr.length; i++) {
+    const str = strarr[i];
 
+    switch (str.length) {
+      case 2:
+        result.set(1, str);
+        break;
+      case 3:
+        result.set(7, str);
+        break;
+      case 4:
+        result.set(4, str);
+        break;
+      case 5:
+        for (const digit of segsByLen.get(5)) {
+          if (!result.get(digit)) {
+            result.set(digit, [str]);
+          } else {
+            let arr = result.get(digit);
+            arr.push(str);
+            result.set(digit, arr);
+          }
+        }
+        break;
+      case 6:
+        for (const digit of segsByLen.get(6)) {
+          if (!result.get(digit)) {
+            result.set(digit, [str]);
+          } else {
+            let arr = result.get(digit);
+            arr.push(str);
+            result.set(digit, arr);
+          }
+        }
+        break;
+      case 7:
+        result.set(8, str);
+        break;
+      default:
+        console.log('invalid str length passed to getLegend')
+    }
+  }
+
+  // 9 condition - includes all '4' segments
+  let setVal = result.get(9).filter(str => strIncludes(str, result.get(4)));
+  result.set(9, setVal[0]);
+
+  // 3 condition - includes both '1' segments
+  setVal = result.get(3).filter(str => strIncludes(str, result.get(1)));
+  result.set(3, setVal[0]);
+
+  //2 condition - 9 array does not include all segments
+  setVal = result.get(2).filter(str => !strIncludes(result.get(9), str))
+  result.set(2, setVal[0]);
+
+  //5 condition - not 2 or 3
+  setVal = result.get(5).filter(str =>
+    (!isAnagram(str, result.get(2)) && !isAnagram(str, result.get(3))))
+  result.set(5, setVal[0])
+
+  // 6 condition - does not include both '1' segments
+  setVal = result.get(6).filter(str => !strIncludes(str, result.get(1)));
+  result.set(6, setVal[0]);
+  // 0 condition - not 6 or 9
+  setVal = result.get(0).filter(str => (!isAnagram(str, result.get(6)) && !isAnagram(str, result.get(9))));
+
+  result.set(0, setVal[0]);
+  return result;
 }
 
 function getOutput(strarr, legend) {
   const digits = [];
-  for(const str of strarr){
-    for(const key of legend.keys()){
-      if(isAnagram(key, str)){
-        digits.push(legend[key]);
+  for (const str of strarr) {
+    for (const [key, val] of legend.entries()) {
+      if (isAnagram(val, str)) {
+        digits.push(key);
         break;
       }
     }
   }
-  return 1111;
+  return parseInt(digits.join(''));
 }
-module.exports = { part1, part2, parse, getData, isAnagram, getLegend, getOutput }
+
+function subLetters(str1, str2) {
+  str1let = str1.split('');
+  str2let = str2.split('');
+  return str1let.filter(letter =>
+    !str2let.includes(letter)).join();
+}
+
+function strIncludes(str1, str2) {
+  return str2.split('').every(letter => str1.split('').includes(letter));
+};
+
+module.exports = { part1, part2, parse, getData, isAnagram, getLegend, getOutput, subLetters }
--- 2021/08/display.test.js
+++ 2021/08/display.test.js
@@ -1,4 +1,4 @@
-const { part1, part2, parse, getData, isAnagram, getLegend, getOutput } = require('./display.js');
+const { part1, part2, parse, getData, isAnagram, getLegend, getOutput, subLetters } = require('./display.js');
 const path = require('path');
 const { abort } = require('process');
 test(
@@ -11,7 +11,7 @@ test(
 test(`parse(str) should return map of input => output signals`, () => {
   const testee = `be cfbegad cbdgef | fdgacbe cefdb
   be cfbegad cbdgef | fdgacbe cefdb`;
-  const result =  new Map ([[["be", "cfbegad", "cbdgef"], ["fdgacbe", "cefdb"]], [["be", "cfbegad", "cbdgef"],["fdgacbe", "cefdb"]]])
+  const result = new Map([[["be", "cfbegad", "cbdgef"], ["fdgacbe", "cefdb"]], [["be", "cfbegad", "cbdgef"], ["fdgacbe", "cefdb"]]])
   expect(parse(testee)).toEqual(result);
 })
 
@@ -19,16 +19,32 @@ test(`isAnagram(abc, cba returns true)`, () => {
   expect(isAnagram('abc', 'cba')).toEqual(true);
 })
 
-test('getLegend() should return 10 digits as keys with null vals', ()=>{
-  const testee = getLegend();
-  for(let i = 0; i < testee.size; i++){
-    expect(testee.get(i)).toEqual(null);
-  }
+
+test('getOutput(["ab", "ab", "ba", "ab"], Map{ 1 => "ab"}) to return 1111', () => {
+  const testee = getOutput(["ab", "ab", "ba", "ab"], new Map([[1, 'ab']]));
+  expect(testee).toEqual(1111)
 })
 
-test('getOutput(["ab", "ab", "ba", "ab"], Map{"ab" => 1}) to return 1111', () => {
-  const testee = getOutput(["ab", "ab", "ba", "ab"], new Map([['ab', 1]]));
-  expect(testee).toEqual(1111)
+test('subLetters(abc, ca) should return b', () => {
+  expect(subLetters('abc', 'ca')).toEqual('b');
+})
+
+describe('get Legend should decipher a map of 10 strings to keys 0-9', () => {
+
+  it('getLegend() should return 10 digits as keys with null vals', () => {
+    const testee = getLegend();
+    for (let i = 0; i < testee.size; i++) {
+      expect(testee.get(i)).toEqual(null);
+    }
+  })
+
+  it('getLegend(test text) should return map of 10 strings', () => {
+    const testText = 'acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab';
+    const vals = testText.split(' ');
+    const tester = new Map([[8, 'acedgfb'], [5, 'cdfbe'], [2, 'gcdfa'], [3, 'fbcad'], [7, 'dab'], [9, 'cefabd'], [6 , 'cdfgeb'], [4, 'eafb'], [0, 'cagedb'], [1, 'ab']])
+    const testee = getLegend(vals);
+    expect(testee).toEqual(tester);
+  })
 })
 
 describe('Main Puzzle Functions', () => {
@@ -38,9 +54,9 @@ describe('Main Puzzle Functions', () => {
     expect(part1(exampleTxt)).toEqual(26);
   });
 
-  test('part2(p2small) should equal 5353', async () => {
+  test('part2(p2small) should equal 61229', async () => {
     const p2small = await getData(path.join(__dirname, 'example.txt'));
-    expect(part2(p2small)).toEqual(5353);
+    expect(part2(p2small)).toEqual(61229);
   });
 
 })
