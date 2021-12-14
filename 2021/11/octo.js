a4bc1fd6b745c07cbcd476a360e3f193c04e8260
zenlex <erichrkeil@gmail.com>
2021-12-14 15:57:56 -0600

    Day 11 Complete

--- 2021/11/octo.js
+++ 2021/11/octo.js
@@ -1,16 +1,150 @@
 const fs = require('fs');
 const path = require('path');
 
-fs.readFile(path.join(__dirname, 'input.txt'), 'utf-8', (err, data) =>{
-  if(err) console.error(err);
+//TODO - WRITE TESTS AGAINST THE SAMPLE INPUT....YOU"RE CLOSE>>>>JUST TEST / DEBUG IT
 
-  if(require.main === module){
+// define grid
+const rows = 10;
+const cols = 10;
+
+fs.readFile(path.join(__dirname, 'input.txt'), 'utf-8', (err, data) => {
+  if (err) console.error(err);
+
+  if (require.main === module) {
     part1(data);
+    part2(data);
   }
 })
 
-function part1(puzzleInput){
-  return 1656; //stub
+let energies;
+
+let flashCount = 0;
+
+const increaseAll = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] += 1;
+  }
+  return arr;
+}
+const resetFlashers = (arr) => {
+  for (let i = 0; i < arr.length; i++)
+    if (arr[i] < 0) {
+      arr[i] = 0;
+    }
+  return arr;
 }
 
-module.exports = { part1 }
+function part1(puzzleInput) {
+  energies = puzzleInput
+    .split('')
+    .map(val => parseInt(val))
+    .filter(val => !isNaN(val));
+
+  function step(numSteps) {
+    while (numSteps > 0) {
+      energies = increaseAll(energies);
+
+      for (let i = 0; i < rows * cols; i++) {
+        energies = flash(i, energies, cols);
+      }
+
+      energies = resetFlashers(energies);
+      numSteps--;
+    }
+  }
+  step(100);
+  console.log('part1 result: ', flashCount)
+  return flashCount;
+}
+
+function part2(puzzleInput) {
+  energies = puzzleInput
+    .split('')
+    .map(val => parseInt(val))
+    .filter(val => !isNaN(val));
+  let stepsTaken = 0;
+  let allFlash = false;
+  function step() {
+    while (!allFlash) {
+      energies = increaseAll(energies);
+
+      for (let i = 0; i < rows * cols; i++) {
+        energies = flash(i, energies, cols);
+      }
+
+      energies = resetFlashers(energies);
+      if(stepsTaken > 10000) break;
+      allFlash = energies.every(val => val === 0);
+      stepsTaken ++;
+    }
+  }
+  step();
+  console.log('part2 result: ', stepsTaken)
+  return stepsTaken;
+}
+
+function flash(octopus, arr, cols) {
+  let energy = arr[octopus];
+  if (energy > 9) {
+    flashCount += 1;
+    arr[octopus] = ~energy;
+    const adjOcts = getAdjacent(octopus, rows, cols);
+    for (const index of adjOcts) {
+      if (arr[index] >= 0) {
+        arr[index] += 1
+        arr = flash(index, arr, cols)
+      }
+    }
+  }
+  return arr;
+}
+
+
+function getAdjacent(octopus, rows, cols) {
+  const adjacents = [];
+  
+  const rightEdge = (i) => (i + 1) % cols === 0;
+  const leftEdge = (i) => i % cols === 0;
+  const topEdge = (i) => i < cols;
+  const bottomEdge = (i) => i >= (rows - 1) * cols;
+
+  //top
+  if (!topEdge(octopus)) {
+    adjacents.push(octopus - cols);
+  }
+  //top right
+  if (!topEdge(octopus) && !rightEdge(octopus)) {
+    adjacents.push(octopus - cols + 1)
+  }
+  //right
+  if (!rightEdge(octopus)) {
+    adjacents.push(octopus + 1);
+  }
+  //bottom right
+  if (!rightEdge(octopus)
+    && !bottomEdge(octopus)) {
+    adjacents.push(octopus + cols + 1)
+  }
+  //bottom
+  if (!bottomEdge(octopus)) {
+    adjacents.push(octopus + cols)
+  }
+  //bottom left
+  if (!leftEdge(octopus) && !bottomEdge(octopus)) {
+    adjacents.push(octopus + cols - 1)
+  }
+  //left
+  if (!leftEdge(octopus)) {
+    adjacents.push(octopus - 1)
+  }
+
+  //top left
+  if (!leftEdge(octopus) && !topEdge(octopus)) {
+    adjacents.push(octopus - cols - 1)
+  }
+
+  return adjacents;
+}
+
+
+module.exports = { part1, part2, increaseAll, resetFlashers, flash, getAdjacent }
--- 2021/11/octo.test.js
+++ 2021/11/octo.test.js
@@ -1,7 +1,7 @@
-const {part1} = require('./octo');
+const { part1, part2, increaseAll, resetFlashers, flash, getAdjacent } = require('./octo');
 
-const sample1 = 
-`5483143223
+const sample1 =
+  `5483143223
 2745854711
 5264556173
 6141336146
@@ -12,8 +12,59 @@ const sample1 =
 4846848554
 5283751526`;
 
-describe('main running tests', () => {
+const rows = 10;
+const cols = 10;
+
+describe('main puzzle solutions', () => {
   it('part1(sample1) should return 1656', () => {
     expect(part1(sample1)).toEqual(1656);
   })
+  it('part2(sample1) should return 195', () => {
+    expect(part2(sample1)).toEqual(195);
+  })
 })
+
+test('increaseEnergy([1,2,3,4,5] should return [2,3,4,5,6]', () => {
+  const testee = [1, 2, 3, 4, 5];
+  const result = [2, 3, 4, 5, 6];
+  expect(increaseAll(testee)).toEqual(result);
+})
+
+test('resetFlashers([-10, 2, 4, -10, 6] should return [0, 2, 4, 0, 6]', () => {
+  const testee = [-10, 2, 4, -10, 6];
+  const result = [0, 2, 4, 0, 6];
+  expect(resetFlashers(testee)).toEqual(result);
+})
+
+test('flash(4, [1, 1, 1, 1, 10, 1, 1, 1, 1] should flash once', () => {
+  const cols = 3;
+  const testee = [1, 1, 1,
+    1, 10, 1,
+    1, 1, 1];
+  const result = [2, 2, 2,
+    2, -11, 2,
+    2, 2, 2
+  ]
+
+  expect(flash(4, testee, cols)).toEqual(result);
+})
+
+test('getAdjacent(4, 3, 3) returns surrounding indexes', () => {
+  expect(getAdjacent(4, 3, 3)).toEqual([1, 2, 5, 8, 7, 6, 3, 0]);
+})
+/**
+ * Pseudo testing
+ * step(index) 
+ *  - use to iterate over all indexes
+ * increase(index)
+ *  - increases non-negative value at each index by 1.
+ *  - 
+ *  flash(index)
+ *  - calls 'flash(octopus)' for every octopus > 9(recursively)
+ *  - flags that octopus as flashed by using bitwise NOT (~)
+ *  - adds one to flash count
+ *  - calls increase on all adjacent non-negative indexes
+ * 
+ * getAdjacent(index)
+ * -returns array of adjacent indexes not out of bounds
+ */
