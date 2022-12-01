85f122b5af0ff380002b47cd845e6d7bbced6056
zenlex <erichrkeil@gmail.com>
2022-11-30 20:40:10 -0600

    add namespaces

--- 2022/00.test.ts
+++ 2022/00.test.ts
@@ -1,17 +1,17 @@
-function main(): void {
-	console.log('hello advent typescript');
+namespace template {
+	function main(): void {
+		console.log('hello advent typescript');
+	}
+
+	test('example 1', function (): void {
+		expect(true).toBe(true);
+	});
+
+	test('example 2', function (): void {
+		expect(false).toBe(false);
+	});
+
+	afterAll(main);
+
+	const input: string = '';
 }
-
-test('example 1', function (): void {
-	expect(true).toBe(true);
-});
-
-test('example 2', function (): void {
-	expect(false).toBe(false);
-});
-
-afterAll(main);
-
-const input: string = '';
-
-export { }
