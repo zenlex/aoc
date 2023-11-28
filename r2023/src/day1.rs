85a70462bed595e687eb80fb0e31c936adc00998
Erich Keil <erich.keil@clevyr.com>
2023-11-27 18:21:57 -0600

    chore: cleanup package and setup 2023 rust

---
+++ r2023/src/day1.rs
@@ -0,0 +1,12 @@
+pub fn main() {
+    println!("Hello, Day 1 AOC!");
+}
+
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_day1() {
+        assert_eq!(true, false);
+    }
+}
---
+++ r2023/src/lib.rs
@@ -0,0 +1,4 @@
+mod day1;
+
+#[cfg(test)]
+mod tests {}
