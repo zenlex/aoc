d81637e538e2e1be970037d656b658b6534a74b2
zenlex <erichrkeil@gmail.com>
2023-12-01 22:06:19 -0600

    d1 complete

--- r2023/src/day1.rs
+++ r2023/src/day1.rs
@@ -1,12 +1,58 @@
-pub fn main() {
-    println!("Hello, Day 1 AOC!");
+use std::error::Error;
+
+use crate::utils;
+pub fn run() {
+    println!("Running D1...");
+    println!("Part 1: {}", main("./inputs/d1.txt", false).unwrap());
+    println!("Part 2: {}", main("./inputs/d1.txt", true).unwrap());
 }
 
+fn main(input: &str, digitwords: bool) -> Result<i32, Box<dyn Error>> {
+    let lines = utils::read_lines(input)?.map(|x| x.unwrap());
+    let mut sum = 0;
+    for line in lines {
+        let mut line = line;
+        if digitwords {
+            line = replace_digits(&line)
+        };
+        let mut numbers = line.chars().filter(|x| x.is_numeric());
+        let first_digit = numbers.next().unwrap();
+        let last_digit = match numbers.last() {
+            Some(x) => x,
+            None => first_digit,
+        };
+        let calibration_value = format!("{}{}", first_digit, last_digit).parse::<i32>()?;
+        sum += calibration_value;
+    }
+
+    Ok(sum)
+}
+
+fn replace_digits(input: &str) -> String {
+    input
+        .replace("one", "o1e")
+        .replace("two", "t2o")
+        .replace("three", "t3e")
+        .replace("four", "f4r")
+        .replace("five", "f5e")
+        .replace("six", "s6x")
+        .replace("seven", "s7n")
+        .replace("eight", "e8t")
+        .replace("nine", "n9e")
+        .replace("zero", "z0o")
+}
+
+#[cfg(test)]
 mod tests {
     use super::*;
+    use crate::testing::TestResult;
 
     #[test]
-    fn test_day1() {
-        assert_eq!(true, false);
+    fn example() -> TestResult {
+        assert_eq!(main("./inputs/d1-ex1.txt", false)?, 142);
+        assert_eq!(main("./inputs/d1-ex2.txt", true)?, 281);
+        assert_eq!(main("./inputs/d1.txt", false)?, 54968);
+        assert_eq!(main("./inputs/d1.txt", true)?, 54094);
+        Ok(())
     }
 }
--- r2023/src/lib.rs
+++ r2023/src/lib.rs
@@ -1,4 +1,27 @@
 mod day1;
 
+pub fn run() {
+    day1::run();
+}
+mod utils {
+
+    use std::fmt::Debug;
+    use std::fs::File;
+    use std::io::{self, BufRead};
+    use std::path::Path;
+
+    pub fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+    where
+        P: AsRef<Path> + Debug,
+    {
+        let file = File::open(filename)?;
+        Ok(io::BufReader::new(file).lines())
+    }
+}
+
+#[cfg(test)]
+mod testing {
+    pub type TestResult = Result<(), Box<dyn std::error::Error>>;
+}
 #[cfg(test)]
 mod tests {}
