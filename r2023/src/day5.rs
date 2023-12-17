8b99e6040b743a856575229da39bfdcd753157d4
zenlex <erichrkeil@gmail.com>
2023-12-16 18:43:06 -0600

    feat: p2 example passing and full sample running in time - too high

--- r2023/src/day5.rs
+++ r2023/src/day5.rs
@@ -1,5 +1,8 @@
 use core::panic;
-use std::{cmp::Ordering, error::Error};
+use std::{
+    cmp::{max, min, Ordering},
+    error::Error,
+};
 
 use indexmap::IndexMap;
 
@@ -14,12 +17,7 @@ pub fn run() {
 fn main(input: &str, part: usize) -> Result<usize, Box<dyn Error>> {
     let mut lines = utils::read_lines(input)?.map(|l| l.unwrap());
 
-    let seeds = lines.next().unwrap()[7..]
-        .split(' ')
-        .collect::<Vec<_>>()
-        .iter()
-        .map(|s| s.parse::<usize>().unwrap())
-        .collect();
+    let seeds = Almanac::get_seed_ranges(&lines.next().unwrap()[7..], part);
 
     let maps = [
         "seed-to-soil",
@@ -65,11 +63,11 @@ fn main(input: &str, part: usize) -> Result<usize, Box<dyn Error>> {
         }
     }
 
-    let locations = almanac.get_locations(seeds, part);
+    let locations = almanac.get_locations(seeds);
     Ok(*locations.iter().min().unwrap())
 }
 
-type Seeds = Vec<usize>;
+type Seeds = Vec<(usize, usize)>;
 type Locations = Vec<usize>;
 
 #[derive(Debug, PartialEq, Clone, Copy)]
@@ -102,26 +100,38 @@ impl Almanac {
         }
     }
 
-    fn get_seeds_from_ranges(input: Seeds) -> Seeds {
+    fn get_seed_ranges(input: &str, part: usize) -> Vec<(usize, usize)> {
+        let values: Vec<usize> = input
+            .split(' ')
+            .collect::<Vec<_>>()
+            .iter()
+            .map(|s| s.parse::<usize>().unwrap())
+            .collect();
+
         let mut seeds = vec![];
 
         let mut i = 0;
-        while i < input.len() {
-            let start = input[i];
-            let end = start + input[i + 1];
-            seeds.extend(start..end);
-            i += 2;
+        while i < values.len() {
+            let start = values[i];
+            let end;
+            match part {
+                1 => {
+                    end = start;
+                    i += 1
+                }
+                2 => {
+                    end = start + values[i + 1] - 1;
+                    i += 2
+                }
+                _ => panic!("Invalid part"),
+            };
+            seeds.push((start, end));
         }
 
         seeds
     }
 
-    fn get_locations(&self, seeds: Seeds, part: usize) -> Locations {
-        let seeds = match part {
-            1 => seeds,
-            2 => Self::get_seeds_from_ranges(seeds),
-            _ => panic!("Invalid part"),
-        };
+    fn get_locations(&self, seeds: Seeds) -> Locations {
         let soils = self.transform(seeds, &self.soil_map);
         let fertilizers = self.transform(soils, &self.fertilizer_map);
         let waters = self.transform(fertilizers, &self.water_map);
@@ -129,64 +139,161 @@ impl Almanac {
         let temperatures = self.transform(lights, &self.temperature_map);
         let humidities = self.transform(temperatures, &self.humidity_map);
         self.transform(humidities, &self.location_map)
-    }
-
-    fn transform(&self, input: Vec<usize>, map: &Map) -> Vec<usize> {
-        input
             .iter()
-            .map(|item| match Self::get_offset(*item, map) {
-                Offset::Pos(offset) => item + offset,
-                Offset::Neg(offset) => item - offset,
-            })
+            .map(|item| item.0)
             .collect()
     }
 
-    fn get_offset(needle: usize, haystack: &Map) -> Offset {
-        let mut keys: Vec<_> = haystack.keys().collect();
+    fn transform(&self, input: Vec<(usize, usize)>, map: &Map) -> Vec<(usize, usize)> {
+        //TODO: split ranges - so get nearest lte neighbor and check range
+        // add new range to output for each split
+        let mut keys = map.keys().collect::<Vec<_>>();
         keys.sort();
 
-        if keys.is_empty() {
-            return Offset::Pos(0);
-        }
+        let mut output = vec![];
+        for (start, end) in input {
+            if start < *keys[0] {
+                output.push((start, min(end, *keys[0] - 1)));
+                continue;
+            }
+            let neighbors = Self::get_neighbors(start, &keys);
+            let mut index = start;
 
-        if needle < *keys[0] {
-            return Offset::Pos(0);
-        }
+            if let Some(key) = neighbors.0 {
+                let (offset, range) = map.get(&key).unwrap();
+                while index < key + range && index <= end {
+                    let new_start = index;
+                    let new_end = if end < key + range {
+                        end
+                    } else {
+                        key + range - 1
+                    };
+                    index = max(new_end + 1, key + range);
 
-        if keys.len() < 3 {
-            for i in 0..keys.len() {
-                match needle < keys[i] + haystack[keys[i]].1 && needle >= *keys[i] {
-                    true => return haystack[keys[i]].0,
-                    false => (),
+                    let offset_start = match offset {
+                        Offset::Pos(v) => new_start + v,
+                        Offset::Neg(v) => new_start - v,
+                    };
+
+                    let offset_end = match offset {
+                        Offset::Pos(v) => new_end + v,
+                        Offset::Neg(v) => new_end - v,
+                    };
+
+                    output.push((offset_start, offset_end));
                 }
             }
-            return Offset::Pos(0);
+
+            match neighbors.1 {
+                Some(key) => {
+                    let (offset, range) = map.get(&key).unwrap();
+                    while index >= key && index < key + range && index <= end {
+                        let new_start = index;
+                        let new_end = if end < key + range {
+                            end
+                        } else {
+                            key + range - 1
+                        };
+                        index = max(new_end + 1, key + range);
+
+                        let offset_start = match offset {
+                            Offset::Pos(v) => new_start + v,
+                            Offset::Neg(v) => new_start - v,
+                        };
+
+                        let offset_end = match offset {
+                            Offset::Pos(v) => new_end + v,
+                            Offset::Neg(v) => new_end - v,
+                        };
+
+                        output.push((offset_start, offset_end));
+                    }
+                }
+                None => {
+                    if index <= end {
+                        output.push((index, end));
+                    }
+                }
+            };
+        }
+        output
+    }
+
+    fn get_neighbors(needle: usize, haystack: &Vec<&usize>) -> (Option<usize>, Option<usize>) {
+        if haystack.is_empty() {
+            panic!("No keys to search in map");
+        }
+
+        if haystack.len() == 1 {
+            match needle.cmp(haystack[0]) {
+                Ordering::Less => return (None, Some(*haystack[0])),
+                _ => return (Some(*haystack[0]), None),
+            }
+        }
+
+        if haystack.len() == 2 {
+            match needle.cmp(haystack[0]) {
+                Ordering::Less => return (None, Some(*haystack[0])),
+                Ordering::Equal => return (Some(*haystack[0]), Some(*haystack[1])),
+                Ordering::Greater => match needle.cmp(haystack[1]) {
+                    Ordering::Less => return (Some(*haystack[0]), Some(*haystack[1])),
+                    Ordering::Equal => return (Some(*haystack[1]), None),
+                    Ordering::Greater => return (Some(*haystack[1]), None),
+                },
+            }
         }
 
         let mut left = 0;
         let mut right = haystack.len() - 1;
         let mut pivot = (right + left) / 2;
 
+        let lower;
+        let upper;
+
         while right >= left {
-            let key = keys[pivot];
+            let key = haystack[pivot];
             match key.cmp(&needle) {
-                Ordering::Equal => return haystack.get(&needle).unwrap().0,
+                Ordering::Equal => {
+                    lower = Some(key);
+                    upper = match pivot + 1 < haystack.len() {
+                        true => Some(haystack[pivot + 1]),
+                        false => None,
+                    };
+                    return (lower.copied(), upper.copied());
+                }
                 Ordering::Less => left = pivot + 1,
                 Ordering::Greater => {
                     right = match pivot.checked_sub(1) {
                         Some(v) => v,
-                        None => break,
+                        None => return (None, Some(*haystack[0])),
                     }
                 }
             };
             pivot = (right + left) / 2;
         }
 
-        let (_, range) = haystack.get(keys[pivot]).unwrap();
-        match needle < keys[pivot] + range {
-            true => haystack[keys[pivot]].0,
-            false => Offset::Pos(0),
-        }
+        match needle.cmp(haystack[pivot]) {
+            Ordering::Less => match pivot.checked_sub(1) {
+                Some(v) => {
+                    lower = Some(haystack[v]);
+                    upper = Some(haystack[pivot]);
+                }
+                None => {
+                    lower = None;
+                    upper = None;
+                }
+            },
+            Ordering::Greater => {
+                lower = Some(haystack[pivot]);
+                upper = match pivot + 1 < haystack.len() {
+                    true => Some(haystack[pivot + 1]),
+                    false => None,
+                };
+            }
+            _ => panic!("Invalid pivot"),
+        };
+
+        (lower.copied(), upper.copied())
     }
 }
 
@@ -207,20 +314,31 @@ mod tests {
     }
 
     #[test]
-    fn test_get_offset() {
+    fn test_seed_ranges() {
+        assert_eq!(
+            Almanac::get_seed_ranges("79 14 55 13", 2),
+            vec![(79, 92), (55, 67)]
+        );
+    }
+
+    #[test]
+    fn get_soils() {
         let mut soil_map = IndexMap::new();
-        soil_map.insert(50, (Offset::Pos(2), 48));
         soil_map.insert(98, (Offset::Neg(48), 2));
+        soil_map.insert(50, (Offset::Pos(2), 48));
         let mut almanac = Almanac::new();
         std::mem::swap(&mut almanac.soil_map, &mut soil_map);
         assert_eq!(
-            (almanac.transform(vec![98, 99], &almanac.soil_map)),
-            vec![50, 51]
+            (almanac.transform(
+                vec![(79, 79), (14, 14), (55, 55), (13, 13)],
+                &almanac.soil_map
+            )),
+            vec![(81, 81), (14, 14), (57, 57), (13, 13)]
         );
-
-        assert_eq!(Almanac::get_offset(50, &almanac.soil_map), Offset::Pos(2));
-        assert_eq!(Almanac::get_offset(52, &almanac.soil_map), Offset::Pos(2));
-        assert_eq!(Almanac::get_offset(99, &almanac.soil_map), Offset::Neg(48));
+        assert_eq!(
+            (almanac.transform(vec![(79, 92), (55, 67)], &almanac.soil_map)),
+            vec![(81, 81), (14, 14), (57, 57), (13, 13)]
+        );
     }
 
     #[test]
@@ -232,8 +350,11 @@ mod tests {
         let mut almanac = Almanac::new();
         std::mem::swap(&mut almanac.fertilizer_map, &mut fertilizer_map);
         assert_eq!(
-            (almanac.transform(vec![81, 14, 57, 13], &almanac.fertilizer_map)),
-            vec![81, 53, 57, 52]
+            (almanac.transform(
+                vec![(81, 81), (14, 14), (57, 57), (13, 13)],
+                &almanac.fertilizer_map
+            )),
+            vec![(81, 81), (53, 53), (57, 57), (52, 52)]
         );
     }
 
@@ -247,8 +368,11 @@ mod tests {
         let mut almanac = Almanac::new();
         std::mem::swap(&mut almanac.water_map, &mut water_map);
         assert_eq!(
-            (almanac.transform(vec![81, 53, 57, 52], &almanac.water_map)),
-            vec![81, 49, 53, 41]
+            (almanac.transform(
+                vec![(81, 81), (53, 53), (57, 57), (52, 52)],
+                &almanac.water_map
+            )),
+            vec![(81, 81), (49, 49), (53, 53), (41, 41)]
         );
     }
 
@@ -260,8 +384,11 @@ mod tests {
         let mut almanac = Almanac::new();
         std::mem::swap(&mut almanac.light_map, &mut light_map);
         assert_eq!(
-            (almanac.transform(vec![81, 49, 53, 41], &almanac.light_map)),
-            vec![74, 42, 46, 34]
+            (almanac.transform(
+                vec![(81, 81), (49, 49), (53, 53), (41, 41)],
+                &almanac.light_map
+            )),
+            vec![(74, 74), (42, 42), (46, 46), (34, 34)]
         );
     }
 
@@ -274,8 +401,11 @@ mod tests {
         let mut almanac = Almanac::new();
         std::mem::swap(&mut almanac.temperature_map, &mut temperature_map);
         assert_eq!(
-            (almanac.transform(vec![74, 42, 46, 34], &almanac.temperature_map)),
-            vec![78, 42, 82, 34]
+            (almanac.transform(
+                vec![(74, 74), (42, 42), (46, 46), (34, 34)],
+                &almanac.temperature_map
+            )),
+            vec![(78, 78), (42, 42), (82, 82), (34, 34)]
         );
     }
 }
