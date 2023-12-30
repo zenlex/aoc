c3a418dbacc4bd6b02c755804ef59e58c264a5a6
zenlex <erichrkeil@gmail.com>
2023-12-30 16:10:53 -0600

    chore: d10 setup

--- r2023/src/day10.rs
+++ r2023/src/day10.rs
@@ -3,12 +3,14 @@ use std::error::Error;
 use crate::utils;
 
 pub fn run() {
-    println!("Running D10...");
-    println!("Part 1: {}", main("./inputs/d10.txt").unwrap());
-    // println!("Part 2: {}", main("./inputs/d10.txt").unwrap());
+    println!("Running D...");
+    println!("Part 1: {}", main("./inputs/d1.txt").unwrap());
+    println!("Part 2: {}", main("./inputs/d1.txt").unwrap());
 }
 
-fn main(input: &str) -> Result<i32, Box<dyn Error>> {}
+fn main(input: &str) -> Result<i32, Box<dyn Error>> {
+    Ok(4)
+}
 
 #[cfg(test)]
 mod tests {
@@ -16,7 +18,8 @@ mod tests {
 
     #[test]
     fn test_part1() {
-        assert_eq!(main("./inputs/d10-ex1.txt").unwrap(), 13);
+        assert_eq!(main("./inputs/d10-ex1.txt").unwrap(), 4);
+        // assert_eq!(main("./inputs/d10-ex2.txt").unwrap(), 8);
         // assert_eq!(main("./inputs/d#.txt").unwrap(), 0);
     }
 
--- r2023/src/lib.rs
+++ r2023/src/lib.rs
@@ -1,4 +1,5 @@
 mod day1;
+mod day10;
 mod day2;
 mod day3;
 mod day4;
@@ -9,15 +10,16 @@ mod day8;
 mod day9;
 
 pub fn run() {
-    day1::run();
-    day2::run();
-    day3::run();
-    day4::run();
-    day5::run();
-    day6::run();
-    day7::run();
-    day8::run();
-    day9::run();
+    // day1::run();
+    // day2::run();
+    // day3::run();
+    // day4::run();
+    // day5::run();
+    // day6::run();
+    // day7::run();
+    // day8::run();
+    // day9::run();
+    day10::run();
 }
 mod utils {
 
