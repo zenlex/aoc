05c8e8abb297cedb81a6c60bab2bbd4e87b15b08
zenlex <erichrkeil@gmail.com>
2024-01-07 02:52:25 -0600

    --wip-- d12p1

--- r2023/src/day12.rs
+++ r2023/src/day12.rs
@@ -1,4 +1,6 @@
-use std::error::Error;
+use std::{error::Error, iter::Sum};
+
+use memoize::memoize;
 
 use crate::utils;
 
@@ -8,19 +10,68 @@ pub fn run() {
     // println!("Part 2: {}", main("./inputs/d10.txt").unwrap());
 }
 
-fn main(input: &str) -> Result<i32, Box<dyn Error>> {
+//TODO DEBUG the first example test - should be recursively memoized solution now
+//TODO implement with a DFA
+
+fn main(input: &str) -> Result<usize, Box<dyn Error>> {
     let lines = utils::read_lines(input)?.map(|l| l.unwrap());
-    let mut arrangements = vec![];
+    let mut total = 0;
     for line in lines {
-        arrangements.push(count_arrangements(&line));
+        let parts = line.split(' ').collect::<Vec<&str>>();
+        let springs = parts[0].chars().collect::<Vec<char>>();
+        let runs = parts[1]
+            .split(',')
+            .map(|s| s.parse::<usize>().unwrap())
+            .collect();
+        total += count_arrangements(springs, runs);
     }
 
-    Ok(21)
+    Ok(total)
 }
 
-fn count_arrangements(line: &str) -> usize {
-    //TODO TDD implementing this function
-    1
+#[memoize]
+fn count_arrangements(line: Vec<char>, runs: Vec<usize>) -> usize {
+    if line.is_empty() {
+        if runs.is_empty() {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    if runs.is_empty() {
+        if line.iter().any(|c| *c == '#') {
+            return 0;
+        }
+        return 1;
+    }
+
+    if line.len() < (runs.iter().sum::<usize>() + runs.len() - 1) {
+        return 0;
+    }
+
+    if line[0] == '.' {
+        return count_arrangements(line[1..].to_vec(), runs);
+    }
+    if line[0] == '#' {
+        let run = runs[0];
+        let leftover_runs = runs[1..].to_vec();
+        for i in 0..run {
+            if line[i] == '.' {
+                return 0;
+            }
+        }
+
+        if (line[run] == '#') {
+            return 0;
+        }
+
+        return count_arrangements(line[1..].to_vec(), leftover_runs);
+    }
+
+    let mut new_line = line[1..].to_vec();
+    new_line.insert(0, '#');
+    count_arrangements(new_line, runs)
 }
 
 #[cfg(test)]
@@ -41,6 +92,9 @@ mod tests {
 
     #[test]
     fn counts() {
-        assert_eq!(count_arrangements("???.### 1,1,3"), 1);
+        assert_eq!(
+            count_arrangements("???.###".chars().collect(), vec![1, 1, 3]),
+            1
+        );
     }
 }
