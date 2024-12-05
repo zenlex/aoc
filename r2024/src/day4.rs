29ce5bbbd626d7db3f72b8d33a7489fbcd710a92
zenlex <erichrkeil@gmail.com>
2024-12-04 22:29:26 -0600

    d4 refactor points and vectors to simplify

--- r2024/src/day4.rs
+++ r2024/src/day4.rs
@@ -3,36 +3,13 @@ use aoc_utils::*;
 use anyhow::Result;
 
 #[derive(Copy, Clone, Debug, Eq, PartialEq)]
-struct Point(usize, usize);
+struct Point(i32, i32);
 
-struct DirectionsIter {
-    index: usize
-}
-
-impl DirectionsIter {
-    pub fn new()->Self {
-        DirectionsIter {index:0}
-    }
-}
-impl Iterator for DirectionsIter {
-    type Item = (i32, i32);
-
-    fn next(&mut self) -> Option<Self::Item> {
-        let directions = [
-            (-1,-1),(-1,0),(-1,1),
-            (0,-1),        (0, 1),
-            (1,-1), (1,0), (1, 1)
-        ];
-
-        if self.index < directions.len() {
-            let direction = directions[self.index];
-            self.index += 1;
-            Some(direction)
-        }else {
-            None
-        }
-    }
-}
+const DIRECTIONS:[(i32,i32);8] = [
+    (-1,-1),(-1,0),(-1,1),
+    (0,-1),        (0, 1),
+    (1,-1), (1,0), (1, 1)
+];
 #[allow(dead_code)]
 pub fn run() {
     println!("Running D4...");
@@ -60,7 +37,7 @@ fn count_all_xmas(grid: CharGrid) -> Result<i32>
         for c in 0..width {
            match grid[r][c] {
                'X' => {
-                   total += count_xmas_at_pos(&grid, Point(r, c))
+                   total += count_xmas_at_pos(&grid, Point(r as i32, c as i32))
                }
                _ => {}
            }
@@ -74,14 +51,14 @@ fn count_xmas_at_pos(grid: &CharGrid, start: Point) -> i32
 {
     let mut count = 0;
 
-   for direction in DirectionsIter::new(){
+   for direction in DIRECTIONS.iter(){
         if is_xmas(grid, start, direction) {count += 1}
    }
 
     count
 }
 
-fn is_xmas(grid:&CharGrid, start: Point, direction: (i32, i32)) -> bool
+fn is_xmas(grid:&CharGrid, start: Point, direction: &(i32, i32)) -> bool
 {
     let mut expected = 'M';
     let mut search = start;
@@ -90,16 +67,16 @@ fn is_xmas(grid:&CharGrid, start: Point, direction: (i32, i32)) -> bool
         if expected == '*' {return true};
         if expected == '.' {return false};
 
-        let new_row = search.0 as i32 + direction.0;
-        let new_col = search.1 as i32 + direction.1;
+        let new_row = search.0 + direction.0;
+        let new_col = search.1 + direction.1;
 
         if new_row < 0 || new_row >= grid.len() as i32 {return false;}
         if new_col < 0 || new_col >= grid[0].len() as i32 {return false;}
 
-        search.0 = new_row as usize;
-        search.1 = new_col as usize;
+        search.0 = new_row;
+        search.1 = new_col;
 
-        let current_char = grid[search.0][search.1];
+        let current_char = grid[search.0 as usize][search.1 as usize];
         if current_char != expected {return false;}
 
        expected = match expected {
@@ -121,7 +98,7 @@ fn count_all_x_mas(grid: &CharGrid) -> i32
     // can count in 1 from edge for A's
     for r in 1..height - 1 {
         for c in 1..width - 1 {
-            if grid[r][c] == 'A' && is_x_mas(grid, Point(r,c)) { total += 1 }
+            if grid[r][c] == 'A' && is_x_mas(grid, Point(r as i32,c as i32)) { total += 1 }
         }
     }
 
@@ -130,10 +107,10 @@ fn count_all_x_mas(grid: &CharGrid) -> i32
 
 fn is_x_mas(grid: &CharGrid, start: Point) -> bool
 {
-    let tlc = grid[start.0 - 1][start.1 - 1];
-    let trc = grid[start.0 - 1][start.1 + 1];
-    let blc = grid[start.0 + 1][start.1 - 1];
-    let brc = grid[start.0 + 1][start.1 + 1];
+    let tlc = grid[(start.0 - 1) as usize][(start.1 - 1) as usize];
+    let trc = grid[(start.0 - 1) as usize][(start.1 + 1) as usize];
+    let blc = grid[(start.0 + 1) as usize][(start.1 - 1) as usize];
+    let brc = grid[(start.0 + 1) as usize][(start.1 + 1) as usize];
 
     (tlc == 'M' && brc == 'S' || tlc == 'S' && brc == 'M')
     && (trc == 'M' && blc == 'S' || trc == 'S' && blc == 'M')
