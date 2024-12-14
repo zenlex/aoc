640cd924c7e77d8d53b1549cea444d6850b4218f
zenlex <erichrkeil@gmail.com>
2024-12-13 23:46:54 -0600

    ts2024 setup

---
+++ ts2024/dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+function main() {
+    console.log("Hello TS World");
+}
+main();
+//# sourceMappingURL=index.js.map
---
+++ ts2024/string.extensions.ts
@@ -0,0 +1,46 @@
+interface String {
+	toChunks(separator?: string): string[],
+	toLines(separator?: string): string[],
+	chunksAndLines(): string[][],
+	alphaGrid(): string[][],
+	toNumericChunks(separator?: string): number[],
+	toNumericLines(separator?: string): number[],
+	numericChunksAndLines(): number[][],
+	numbericGrid(): number[][]
+}
+
+String.prototype.toChunks = function (separator: string = '\n\n'): string[] {
+	return this.split(separator);
+}
+
+String.prototype.toLines = function (separator: string = '\n'): string[] {
+	return this.split(separator);
+}
+
+String.prototype.chunksAndLines = function (): string[][] {
+	return this.toChunks().map(chunk => chunk.toLines());
+}
+
+String.prototype.alphaGrid = function (separator: string = ''): string[][] {
+	return this.toLines().map(line => line.split(separator));
+}
+
+
+
+String.prototype.toNumericChunks = function (separator: string = '\n\n'): number[] {
+	return this.split(separator).map(str => parseInt(str));
+}
+
+String.prototype.toNumericLines = function (separator: string = '\n'): number[] {
+	return this.split(separator).map(str => parseInt(str));
+}
+
+String.prototype.numericChunksAndLines = function (): number[][] {
+	return this.toChunks().map(chunk => chunk.toNumericLines());
+}
+
+String.prototype.numbericGrid = function (separator: string = ''): number[][] {
+	return this.toLines().map(line => {
+		return line.split(separator).map(val => parseInt(val))
+	});
+}
