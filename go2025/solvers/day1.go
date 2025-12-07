824094149eb50ab0224de46198bd16ba1744ae1e
zenlex <erichrkeil@gmail.com>
2025-12-07 17:27:39 -0600

    feat: d1p1 complete

--- go2025/solvers/day1.go
+++ go2025/solvers/day1.go
@@ -1,8 +1,60 @@
 package solvers
 
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
 func SolveD1(input string) (int, int) {
-	result := 42
-	return result, 0
+	_, debug := os.LookupEnv("DEBUG")
+	current := 50
+	zeros := 0
+
+	instructions := strings.Split(input, "\n")
+
+	for _, instruction := range instructions {
+		instruction := []rune(instruction)
+		if len(instruction) == 0 {
+			continue
+		}
+		direction := string(instruction[0])
+		amountStr := strings.TrimSpace(string(instruction[1:]))
+		amount, _ := strconv.Atoi(amountStr)
+
+		if debug {
+			fmt.Printf("direction:%s amount:%v\n", direction, amount)
+		}
+		amount = amount % 100
+
+		switch direction {
+		case "L":
+			current = current - amount
+			if debug {
+				fmt.Printf("instruction: %s, result:%d\n", string(instruction), current)
+			}
+		case "R":
+			current = current + amount
+			if debug {
+				fmt.Printf("instruction: %s, result:%d\n", string(instruction), current)
+			}
+		}
+
+		for current < 0 {
+			current += 100
+		}
+
+		for current > 99 {
+			current -= 100
+		}
+
+		if current == 0 {
+			zeros++
+		}
+	}
+
+	return zeros, 0
 }
 
 func init() {
